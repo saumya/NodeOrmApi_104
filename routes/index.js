5e9dbc62b753e173c12b15a0a448e6e071f88567
saumya <2saumyaray@gmail.com>
2020-01-21 20:23:05 +0530

    wip: the Database setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express | Sequelize | API' });
+});
+
+router.get('/ormCheck',function(request,response,next){
+	//const result = connCheck();
+	response.send( JSON.stringify({'Checked':true}) );
 });
 
 module.exports = router;
