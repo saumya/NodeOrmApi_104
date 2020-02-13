128fe547bd2077cd3007034718b859397f1ca5ef
saumya <2saumyaray@gmail.com>
2020-02-13 21:11:09 +0530

    CORS enabled

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// --------- CORS ------------------
+// ref: https://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1234');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+
+// --------- CORS / ------------------
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1', v1Router);
