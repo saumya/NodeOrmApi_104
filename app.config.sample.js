dc473cc55effecbd5cb63cf74fa5632d26ad642c
saumya <2saumyaray@gmail.com>
2020-04-03 22:00:10 +0530

    working in Remote and Local Databse servers

--- app.config.sample.js
+++ app.config.sample.js
@@ -6,6 +6,11 @@ const config = {
 	appConfig: {
 		'version' : '0.1.0'
 	},
+	
+	// ---------------------------------
+	// remote DB
+	// ---------------------------------
+
 	dbConfig:{
 		host: 'dbserver.com',
 		port: '3306',
@@ -13,6 +18,19 @@ const config = {
 		password: 'pw',
 		database: 'dbName'
 	}
+	
+	// ---------------------------------
+	// local DB | localhost
+	// ---------------------------------
+	/*
+	dbConfig:{
+		host: 'localhost',
+		port: '8889',
+		user: 'app_user',
+		password: 'app_pw',
+		database: 'node_app_scheduler'
+	}
+	*/
 }
 
 module.exports = config;
