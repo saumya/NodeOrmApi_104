8f55658653bcdb64d9f9af78811ab101df3f9120
saumya <2saumyaray@gmail.com>
2020-08-29 20:27:50 +0530

    Code cleanup

--- app.js
+++ app.js
@@ -27,43 +27,7 @@ app.use(SaumyaLoggerX);
 
 const SaumyaCORS = require('./saumya/SaumyaCORS');
 app.use(SaumyaCORS);
-
-/*
-// --------- CORS ------------------
-// ref: https://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
-// Add headers
-app.use(function (req, res, next) {
-
-  var origin = req.headers.origin;
-  var allowedDomains = ['http://localhost:3001','http://localhost:8080' ];
-
-  console.log('--- domain -----------');
-  console.log(origin);
-  if(allowedDomains.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  console.log('--- domain / -----------');
-  
-  // Website you wish to allow to connect
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080'); // VueJS App
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001'); // ReactJS App
-  
-  //res.setHeader('Access-Control-Allow-Origin', 'https://doctor.findhealth.today');
-  
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-});
-
-// --------- CORS / ------------------
-*/
-
+//
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1', v1Router);
